11eb055adb3a2dd57141afd2e700230e114733fc
Ivan Panussis C <ivan.panussis.ing@gmail.com>
2023-04-18 01:20:31 +0100

    Incremental Static Regeneration and Generation Added

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -4,11 +4,16 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async( nameOrId:string ) => {
     
-    const { data } = await pokemonApi.get<Pokemon>(`/pokemon/${ nameOrId }`);
-    
-    return {
-        id: data.id,
-        name: data.name,
-        sprites: data.sprites
+    try {
+        const { data } = await pokemonApi.get<Pokemon>(`/pokemon/${ nameOrId }`);
+
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites
+        }
+        
+    } catch (error) {
+        return null;
     }
 }
